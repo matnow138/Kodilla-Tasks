8a72c0e78183a40164982211a6ab09d1f361bd09
Mateusz Nowak <mateuszp.nowak@gmail.com>
2022-09-07 13:44:57 +0200

    22.3

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -1,21 +1,23 @@
 package com.crud.tasks.domain;
 
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
-    @JsonProperty("name")
-    private String name;
+public class CreatedTrelloCard {
+
     @JsonProperty("id")
     private String id;
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("shortUrl")
+    private String shortUrl;
+    @JsonProperty("badges")
+    private TrelloBadgesDto trelloBadgesDto;
+
 }
