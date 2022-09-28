00dfe4af9c0c6c2d44f912a5d0d821d5933af283
unknown <mateuszp.nowak@gmail.com>
2022-09-28 11:28:49 +0200

    23.3

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -11,6 +11,6 @@ public class Mail {
     private final String mailTo;
     private final String subject;
     private final String message;
-    private final Optional<String> toCc;
+    private final String toCc;
 
 }
